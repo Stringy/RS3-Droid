6468adfb3f889bdddd6410e1e49a5820ee96b421
Stringy <hutton.giles@gmail.com>
2013-04-21 11:31:34 +0100

    basic layouts/adapters

--- gen/org/codexplosion/rs3/R.java
+++ gen/org/codexplosion/rs3/R.java
@@ -10,22 +10,43 @@ package org.codexplosion.rs3;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f050001;
+        public static final int dark_gray=0x7f050003;
+        public static final int light_gray=0x7f050002;
+        /**  Colours 
+         */
+        public static final int white=0x7f050000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int feedDetailFrag=0x7f080002;
+        public static final int feedItemDesc=0x7f080004;
+        public static final int feedItemFrag=0x7f080001;
+        public static final int feedItemTitle=0x7f080003;
+        public static final int feedItemWebView=0x7f080005;
+        public static final int feedList=0x7f080006;
+        public static final int menu_settings=0x7f080007;
+        public static final int sepDetailFrag=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_detail=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int feed_list_layout=0x7f030002;
+        public static final int fragment_feed_item_detail=0x7f030003;
+        public static final int fragment_feeds=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_detail=0x7f070000;
+        public static final int activity_main=0x7f070001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_detail=0x7f040003;
     }
     public static final class style {
         /** 
@@ -49,10 +70,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/org/codexplosion/rs3/MainActivity.java
+++ src/org/codexplosion/rs3/MainActivity.java
@@ -1,7 +1,8 @@
 package org.codexplosion.rs3;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,7 +11,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		setTitle("");
 	}
 
 	@Override
@@ -19,5 +19,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
 }
